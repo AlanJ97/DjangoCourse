92883a956a995ef92ca6141344904616342af6b2
Alan <alannavaseg_02@hotmail.com>
2020-11-28 19:33:34 -0600

    Static

--- DjangoProjects/second_project/second_app/views.py
+++ DjangoProjects/second_project/second_app/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<em>Seconds attempt</em>")
+
+def help(request):
+    help_dict = {'help_insert': 'HELP PAGE'}
+    return  render(request, 'app_two/help.html', context=help_dict)
