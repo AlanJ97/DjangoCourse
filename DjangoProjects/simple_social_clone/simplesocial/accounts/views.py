b2ba866ddaad16ade57a688e7b75de09adf073e9
Alan <alannavaseg_02@hotmail.com>
2021-01-28 08:16:26 -0600

    Agregando posts y groups apps y sus templates vacios

--- DjangoProjects/simple_social_clone/simplesocial/accounts/views.py
+++ DjangoProjects/simple_social_clone/simplesocial/accounts/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import login, logout
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 
 from . import forms
 
+class SignUp2(TemplateView):
+    template_name = 'index.html'
 class SignUp(CreateView):
     form_class = forms.UserCreateForm
     success_url = reverse_lazy("login")
-    template_name = "signup.html"
+    template_name = "accounts/signup.html"
